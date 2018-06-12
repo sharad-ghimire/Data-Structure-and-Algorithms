7c1360df99ea2507454ed1f80dca1332c4e5454f
Sharad Ghimire <sharadghimire5551@gmail.com>
2018-06-12 20:51:41 +1000

    Java - LinkedList - Add function Added

--- linked-list/linked-list-in-java/linkedlist.java
+++ linked-list/linked-list-in-java/linkedlist.java
@@ -2,7 +2,27 @@ public class LinkedList {
     private static Node head;
     private static int numNodes;
 
+    public static void main(String[] args) {
+        System.out.println("Just For Test");
+        LinkedList list = new LinkedList(10);
+        list.addAtHead(132);
+        list.addAtHead(456);
+        list.addAtHead(164);
+        list.addAtTail(1332);
+        list.addAtTail(394);
+        list.addAtHead(132);  
+    }
+
+    public LinkedList(Object dat) {
+		head = new Node(dat);
+    }
     
+    public void addAtHead(Object dat) {
+		Node temp = head;
+		head = new Node(dat);
+		head.next = temp;
+		numNodes++;
+	}
 
 
 
