e8465342fa765fcb16cf7059b38a15ec3251c415
Sharad Ghimire <sharadghimire5551@gmail.com>
2018-06-14 20:57:30 +1000

    Stack C++ updated

--- stack/stacks.cpp
+++ stack/stacks.cpp
@@ -1,38 +1,102 @@
-#include <iostream>
-#include <stack>
+// #include <iostream>
+// #include <stack>
  
+// using namespace std;
+ 
+// void showstack(stack <int> gq)
+// {
+//     stack <int> g = gq;
+//     while (!g.empty())
+//     {
+//         cout << '\t' << g.top();
+//         g.pop();
+//     }
+//     cout << '\n';
+// }
+ 
+// int main ()
+// {
+//     stack <int> gquiz;
+//     gquiz.push(10);
+//     gquiz.push(30);
+//     gquiz.push(20);
+//     gquiz.push(5);
+//     gquiz.push(1);
+ 
+//     cout << "The stack gquiz is : ";
+//     showstack(gquiz);
+ 
+//     cout << "\ngquiz.size() : " << gquiz.size();
+//     cout << "\ngquiz.top() : " << gquiz.top();
+ 
+ 
+//     cout << "\ngquiz.pop() : ";
+//     gquiz.pop();
+//     showstack(gquiz);
+ 
+//     return 0;
+// }
+
+
+#include<bits/stdc++.h>
 using namespace std;
  
-void showstack(stack <int> gq)
+#define MAX 1000
+ 
+class Stack
 {
-    stack <int> g = gq;
-    while (!g.empty())
+    int top;
+public:
+    int a[MAX];    //Maximum size of Stack
+ 
+    Stack()  { top = -1; }
+    bool push(int x);
+    int pop();
+    bool isEmpty();
+};
+ 
+bool Stack::push(int x)
+{
+    if (top >= MAX)
     {
-        cout << '\t' << g.top();
-        g.pop();
+        cout << "Stack Overflow";
+        return false;
     }
-    cout << '\n';
+    else
+    {
+        a[++top] = x;
+        return true;
+    }
 }
  
-int main ()
+int Stack::pop()
 {
-    stack <int> gquiz;
-    gquiz.push(10);
-    gquiz.push(30);
-    gquiz.push(20);
-    gquiz.push(5);
-    gquiz.push(1);
+    if (top < 0)
+    {
+        cout << "Stack Underflow";
+        return 0;
+    }
+    else
+    {
+        int x = a[top--];
+        return x;
+    }
+}
  
-    cout << "The stack gquiz is : ";
-    showstack(gquiz);
+bool Stack::isEmpty()
+{
+    return (top < 0);
+}
  
-    cout << "\ngquiz.size() : " << gquiz.size();
-    cout << "\ngquiz.top() : " << gquiz.top();
+// Driver program to test above functions
+int main()
+{
+    struct Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
  
- 
-    cout << "\ngquiz.pop() : ";
-    gquiz.pop();
-    showstack(gquiz);
+    cout << s.pop() << " Popped from stack\n";
  
     return 0;
 }
